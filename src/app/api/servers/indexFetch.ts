572cb7106a97cd13baabaabeca149b7249e5f4d3
simonzyWu <simonwuzy412@gmail.com>
2025-06-21 23:19:29 +0800

    fix: 文章获取优化

--- src/app/api/servers/indexFetch.ts
+++ src/app/api/servers/indexFetch.ts
@@ -1,5 +1,3 @@
-import { useQuery } from "convex/react";
-import { getAllArticles } from "../../../../convex/articles";
 import { ConvexHttpClient } from "convex/browser";
 import { api } from "../../../../convex/_generated/api";
 const convex = new ConvexHttpClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
